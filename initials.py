d4dfc8d9d82f2a7351abb8249babf106d6af6f72
Emmanuel Agbedi <ai.emmanuel.up@outlook.com>
2026-05-30 00:43:18 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File: initials.py
+#Description: A program using only the print function to print out my initals
+#Assignment Number: Assignment 2
+#
+#Name: Emmanuel Agbedi
+#STUDENT ID: 2425402768
+#Email: 2425402768@live.gctu.edu.gh
+#Grader: Emma
+#
+#On my honor, Emmanuel Agbedi,this programming assignment is my own work
+#and I have not provided this code to any other student
+
 
 def main():
-  pass #code goes here
-
+    #The "main()" function is used to organise code into reusable blocks of code. The "def" defines a function
+    print()
+    print("...AEL")
+    print()
+    print(".....AAAAAAAA..........EEEEEEEEEEEE........LLLL.............")
+    print("....AAAA..AAAA.........EEEEEEEEEEEE........LLLL.............")
+    print("...AAAA....AAAA........EEEE................LLLL.............")
+    print("...AAAA....AAAA........EEEE................LLLL.............")
+    print("...AAAAAAAAAAAA........EEEEEEEEEEEE........LLLL.............")
+    print("...AAAAAAAAAAAA........EEEEEEEEEEEE........LLLL.............")
+    print("...AAAA....AAAA........EEEE................LLLL.............")
+    print("...AAAA....AAAA........EEEE................LLLL.............")
+    print("...AAAA....AAAA...**...EEEEEEEEEEEE...**...LLLLLLLLLLLL...**")
+    print("...AAAA....AAAA...**...EEEEEEEEEEEE...**...LLLLLLLLLLLL...**")
 
 
 main()
